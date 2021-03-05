41b6b309f891ad64485a61ed8ba18bb5316a6d95
xiaobing.jin <xiaobing.jin@geely.com>
2021-03-05 19:30:41 +0800

    ok

--- Example/Pods/Target Support Files/Pods-TBIMLibrary_Example/Pods-TBIMLibrary_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-TBIMLibrary_Example/Pods-TBIMLibrary_Example-frameworks.sh
@@ -166,6 +166,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/RealReachability/RealReachability.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SocketRocket/SocketRocket.framework"
   install_framework "${PODS_ROOT}/../../TBIM.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/TBIMLibrary/TBIMLibrary.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYModel/YYModel.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
@@ -174,6 +175,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/RealReachability/RealReachability.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SocketRocket/SocketRocket.framework"
   install_framework "${PODS_ROOT}/../../TBIM.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/TBIMLibrary/TBIMLibrary.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYModel/YYModel.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then

3c71d1742a657f09d1f4ffd90afec679f047cddb
xiaobing.jin <xiaobing.jin@geely.com>
2021-03-09 16:03:41 +0800

    更换库

---
+++ TBIMLibrary/Classes/TBIM.framework/Headers/TBIM.h
@@ -0,0 +1,18 @@
+//
+//  TBIM.h
+//  TBIM
+//
+//  Created by xiaobing on 2020/12/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TBIM.
+FOUNDATION_EXPORT double TBIMVersionNumber;
+
+//! Project version string for TBIM.
+FOUNDATION_EXPORT const unsigned char TBIMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TBIM/PublicHeader.h>
+
+
